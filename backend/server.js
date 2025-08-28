2cb196a6d9fc3a999a71f535437b9def6c98232d
Rahul <rahulp00rtx@gmail.com>
2025-08-28 19:33:06 +0530

    19 . Deploy

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ import cartRoutes from "./routes/cart.routes.js";
 import paymentsRoutes from "./routes/payment.routes.js";
 import couponRoutes from "./routes/coupon.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
+import path from "path";
 
 import { connectDB } from "./db/connect/db_connection.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 const app = epxress();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(epxress.json({ limit: "10mb" }));
@@ -22,5 +26,13 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentsRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(epxress.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 connectDB(app);
